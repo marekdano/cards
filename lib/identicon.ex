2084793fbfcbd666ce109246e9da402654fa5ec1
Marek Dano <mk.dano@gmail.com>
2017-08-16 17:20:38 +0100

    Add 'pick_color' method.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,15 @@ defmodule Identicon do
 	def main(input) do
 		input
 		|> hash_input
+		|> pick_color
+	end
+
+	def pick_color(image) do
+	#	%Identicon.Image{hex: hex_list} = image
+	#	[r, g, b | _tail] = hex_list
+		%Identicon.Image{hex: [r, g, b | _tail]} = image
+
+		[r, g, b]
 	end
 
 	def hash_input(input) do
